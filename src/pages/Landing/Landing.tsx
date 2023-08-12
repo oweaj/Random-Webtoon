7a3835f28c924144f9a68f09164ce4e5969db037
oweaj <qwe1520156@naver.com>
2023-08-12 17:25:33 +0900

    [CHORE] 회원가입, 로그인 페이지 삭제

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -9,12 +9,6 @@ const Landing = () => {
       <Link to="/main">
         <p>메인페이지로 이동</p>
       </Link>
-      <Link to="/signin">
-        <p>로그인페이지로 이동</p>
-      </Link>
-      <Link to="/signup">
-        <p>회원가입페이지로 이동</p>
-      </Link>
     </div>
   );
 };
