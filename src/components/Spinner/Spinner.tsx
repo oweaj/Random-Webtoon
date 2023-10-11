a63a553e81c5554975b1152aca7c04a150e523d5
oweaj <qwe1520156@naver.com>
2023-10-12 00:09:25 +0900

    [FEAT] 로딩 스피너 추가

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,17 @@
+import { CSSProperties } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override: CSSProperties = {
+  display: "block",
+  borderColor: "#ea8e16",
+};
+
+const Spinner = () => {
+  return (
+    <div className={`w-full flex items-center justify-center`}>
+      <ClipLoader color="#0000" cssOverride={override} size={80} aria-label="Loading Spinner" data-testid="loader" />
+    </div>
+  );
+};
+
+export default Spinner;
